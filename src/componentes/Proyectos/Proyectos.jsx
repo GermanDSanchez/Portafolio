02c45184812b10f74de4c17ab635e48bd2876b45
GermanDSanchez <germansr02@gmail.com>
2023-07-24 20:12:43 -0300

    commit

--- src/componentes/Proyectos/Proyectos.jsx
+++ src/componentes/Proyectos/Proyectos.jsx
@@ -14,6 +14,7 @@ import primerapi from '../../../public/video/primerapi.webm'
 import maquetaecommerce from '../../../public/video/maquetaecommerce.webm'
 import apimovie from '../../../public/video/apimovie.webm'
 import portafolio from '../../../public/video/portafolio.webm'
+import reto from '../../../public/video/reto.mp4'
 import { useState, useEffect, useRef } from 'react'
 
 // eslint-disable-next-line react/prop-types
@@ -91,18 +92,18 @@ export default function Proyectos () {
             <div className='flex flex-col items-center max-w-[1400px]'>
                 <h2 className="text-5xl mb-12">Proyectos</h2>
                 <div className='flex flex-wrap justify-center gap-10 max-[472px]:gap-6'>
-                    <Proyecto lenguaje1={react} lenguaje2={tailwind} lenguaje3={''} title={'Portafolio (5° Proyecto)'} imagen={<span className='text-4xl font-bold flex flex-col'>Portafolio<span className="p-1 text-black uppercase bg-yellow-400 rounded-lg">German</span></span>}
+                    <Proyecto lenguaje1={react} lenguaje2={tailwind} title={'Portafolio (5° Proyecto)'} imagen={<span className='text-4xl font-bold flex flex-col'>Portafolio<span className="p-1 text-black uppercase bg-yellow-400 rounded-lg">German</span></span>}
                     description="Portafolio (Responsive Design) sobre mis proyectos, avances y contacto, hecho en React y Tailwind" zoom={'xl:translate-x-[135px] xl:translate-y-40'} videoproject={portafolio}/>
-                    <Proyecto lenguaje1={react} lenguaje2={tailwind} lenguaje3={''} title={'APIMovies (4° Proyecto)'} imagen={<img className='w-52' src={apimovies}/>}
+                    <Proyecto lenguaje1={react} lenguaje2={tailwind} title={'APIMovies (4° Proyecto)'} imagen={<img className='w-52' src={apimovies}/>}
                     description="Pagina Web (Desktop) sobre peliculas y series usando la API de TheMovieDB, hecho en React y Tailwind" zoom={'xl:-translate-x-[345px] xl:translate-y-40'} videoproject={apimovie}/>
-                    <Proyecto lenguaje1={react} lenguaje2={css3} lenguaje3={''} title={'Maqueta/Clon Google (3° Proyecto)'} imagen={<Google />}
+                    <Proyecto lenguaje1={react} lenguaje2={css3} title={'Maqueta/Clon Google (3° Proyecto)'} imagen={<Google />}
                     description="Maqueta/Clon del buscador de Google (Desktop), hecho en React y CSS" zoom={'xl:-translate-x-[825px] xl:translate-y-40'} videoproject={maquetagoogle}/>
                     <Proyecto lenguaje1={javass} lenguaje2={css3} lenguaje3={html5} title={'Uso de API/Fetch (2° Proyecto)'} imagen={<img className='w-44' src={api}/>}
                     description="Llamado a API/uso de Fetch basico, hecho con HTML, CSS y Javascript nativo" zoom={'xl:translate-x-[135px] xl:-translate-y-[225px]'} videoproject={primerapi}/>
                     <Proyecto lenguaje1={javass} lenguaje2={css3} lenguaje3={html5} title={'Maqueta E-Commerce (1° Proyecto)'} imagen={<Bike />}
                     description="Maqueta de E-Commerce (Responsive Design) sobre bicicletas, hecho exclusivamente en HTML, CSS y Javascript nativo" zoom={'xl:-translate-x-[345px] xl:-translate-y-[225px]'} videoproject={maquetaecommerce}/>
-                    <Proyecto lenguaje1={''} lenguaje2={javass} lenguaje3={''} title={'Retos de Javascript'} imagen={<span className='text-5xl font-bold'>Retos<span className="p-1 text-black uppercase bg-yellow-400 rounded-lg">js</span></span>}
-                    description="Retos de Javascript que voy realizando de diferentes paginas web" zoom={'xl:-translate-x-[825px] xl:-translate-y-[225px]'} videoproject={apimovie}/>
+                    <Proyecto lenguaje2={javass} title={'Retos de Javascript'} imagen={<span className='text-5xl font-bold'>Retos<span className="p-1 text-black uppercase bg-yellow-400 rounded-lg">js</span></span>}
+                    description="Retos de Javascript que voy realizando de diferentes paginas web" zoom={'xl:-translate-x-[825px] xl:-translate-y-[225px]'} videoproject={reto}/>
                 </div>
             </div>
         </section>
